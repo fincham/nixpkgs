3ed90fb67b91a9c5d80358a223f0514cf5fc2adf
Michael Fincham <michael@hotplate.co.nz>
2020-05-10 16:14:25 +1200

    Updates powerdns service for greater flexibility, sync unit with upstream
    Makes a few updates to the powerdns service module:
    
    - Configuration is now specified with a "config" attr set, though
      extraConfig as a string continues to be supported but is marked as
      deprecated.
    
    - Defaults to running the daemon as "pdns" uid/gid and ensures those
      users exist on the system. This is a change from prior where daemon
      would only run as "nobody".
    
      This shouldn't break existing setups on upgrade as the new "pdns" user
      should have the same access as the existing "nobody" user.
    
    - The pdnsutil(1) and pdns_control(1) command line utilities are now
      installed in the system environment with wrappers to ensure they access
      the correct socket and configuration locations.
    
    - Re-syncs the systemd unit file with upstream to integrate the changes
      they've made since the last update to this service.
    
      Notably this disables chroot as this is no longer recommended by
      upstream, but it does introduce additional hardening measures.
    
    Tested against the existing PowerDNS 4.2.1 in nixpkgs. This is my first
    contribution to NixOS so feedback is welcome :)

--- nixos/modules/services/networking/powerdns.nix
+++ nixos/modules/services/networking/powerdns.nix
@@ -4,46 +4,112 @@ with lib;
 
 let
   cfg = config.services.powerdns;
-  configDir = pkgs.writeTextDir "pdns.conf" "${cfg.extraConfig}";
+
+  socketDir = "/run";
+  configDir = pkgs.writeTextDir "pdns.conf" (generators.toKeyValue { } cfg.config + optionalString (cfg.extraConfig != null) cfg.extraConfig);
+  
+  powerdns-cli-wrappers = pkgs.stdenv.mkDerivation { 
+    name = "powerdns-cli-wrappers"; 
+    buildInputs = [ pkgs.makeWrapper ]; 
+    buildCommand = '' 
+      mkdir -p $out/bin 
+      makeWrapper ${cfg.package}/bin/pdnsutil "$out/bin/pdnsutil" --add-flags "--config-dir=${configDir}"
+      makeWrapper ${cfg.package}/bin/pdns_control "$out/bin/pdns_control" --add-flags "--config-dir=${configDir} --socket-dir=${socketDir}" 
+    ''; 
+  };
+
 in {
+  disabledModules = [ "services/networking/powerdns.nix" ];
+
   options = {
     services.powerdns = {
-      enable = mkEnableOption "Powerdns domain name server";
+      enable = mkEnableOption "PowerDNS Authoritative Server";
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.powerdns;
+        defaultText = "pkgs.powerdns";
+        description = "Which PowerDNS package to use";
+      };
+
+      config = mkOption {
+        type = with types; attrsOf (oneOf [ bool int str ]);
+        default = {
+            launch = "bind";
+        };
+        example = {
+            launch = "bind";
+        };
+        description = "Configuration for pdns_server";
+      };
+
+      user = mkOption {
+        default = "pdns";
+        description = "UID which pdns_server will switch to after starting";
+      };
+
+      group = mkOption {
+        default = "pdns";
+        description = "GID which pdns_server will switch to after starting";
+      };
+
+      extraArgs = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        description = "List of additional command line paramters for pdns_server";
+      };
 
       extraConfig = mkOption {
-        type = types.lines;
-        default = "launch=bind";
-        description = ''
-          Extra lines to be added verbatim to pdns.conf.
-          Powerdns will chroot to /var/lib/powerdns.
-          So any file, powerdns is supposed to be read,
-          should be in /var/lib/powerdns and needs to specified
-          relative to the chroot.
-        '';
+        type = with types; nullOr lines;
+        default = null;
+        description = "Extra lines to be added verbatim to pdns.conf";
       };
     };
   };
 
   config = mkIf config.services.powerdns.enable {
+    warnings = optional (cfg.extraConfig != null) "services.powerdns.`extraConfig` is deprecated, please use services.powerdns.`config`.";
+
+    users.users.pdns = {
+      isSystemUser = true;
+      group = "pdns";
+      description = "PowerDNS daemon user";
+    };
+
+    users.groups.pdns.gid = null;
+
+    # preferably this should be "type=notify" to better match upstream, but this is not currently reliable on NixOS
     systemd.services.pdns = {
-      unitConfig.Documentation = "man:pdns_server(1) man:pdns_control(1)";
-      description = "Powerdns name server";
-      wantedBy = [ "multi-user.target" ];
-      after = ["network.target" "mysql.service" "postgresql.service" "openldap.service"];
+      unitConfig.Documentation = "man:pdns_server(1) man:pdns_control(1) man:pdnsutil(1)";
+      description = "PowerDNS Authoritative Server";
+      wantedBy = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" "mysqld.service" "postgresql.service" "slapd.service" "mariadb.service" ];
 
       serviceConfig = {
-        Restart="on-failure";
-        RestartSec="1";
-        StartLimitInterval="0";
-        PrivateDevices=true;
-        CapabilityBoundingSet="CAP_CHOWN CAP_NET_BIND_SERVICE CAP_SETGID CAP_SETUID CAP_SYS_CHROOT";
-        NoNewPrivileges=true;
-        ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /var/lib/powerdns";
-        ExecStart = "${pkgs.powerdns}/bin/pdns_server --setuid=nobody --setgid=nogroup --chroot=/var/lib/powerdns --socket-dir=/ --daemon=no --guardian=no --disable-syslog --write-pid=no --config-dir=${configDir}";
-        ProtectSystem="full";
-        ProtectHome=true;
-        RestrictAddressFamilies="AF_UNIX AF_INET AF_INET6";
+        ExecStart = "${cfg.package}/bin/pdns_server --setuid=${cfg.user} --setgid=${cfg.group} --guardian=no --daemon=no --disable-syslog --log-timestamp=no --write-pid=no --socket-dir=${socketDir} --config-dir=${configDir} ${concatStringsSep " " cfg.extraArgs}";
+        Type = "simple";
+        Restart = "on-failure";
+        RestartSec = "1";
+        StartLimitInterval = "0";
+        CapabilityBoundingSet = "CAP_NET_BIND_SERVICE CAP_SETGID CAP_SETUID CAP_CHOWN CAP_SYS_CHROOT";
+        LockPersonality = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "full";
+        RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6";
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = "~ @clock @debug @module @mount @raw-io @reboot @swap @cpu-emulation @obsolete";
+        PrivateDevices = true;
+        NoNewPrivileges = true; 
       };
     };
+    
+    environment.systemPackages = [ powerdns-cli-wrappers ];
   };
+
 }
